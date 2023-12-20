e8fd922aef1d682609a6b107a95b46dd9354326f
unknown <edoyangz@gmail.com>
2023-12-20 17:52:25 +1100

    woiii

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
+// require('dotenv').config(); // Local environment
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
-  }
+}
 const express = require('express');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
