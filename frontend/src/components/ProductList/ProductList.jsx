9d3d3fe8207acaa1e0d43afad2746006d35bc1f6
unknown <edoyangz@gmail.com>
2023-12-27 20:05:03 +1100

    Style: Add responsive

--- frontend/src/components/ProductList/ProductList.jsx
+++ frontend/src/components/ProductList/ProductList.jsx
@@ -1,13 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ProductCard from '../ProductCard/ProductCard';
+import './index.scss';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // Added loading state
+  const productListRef = useRef(null); // Ref for the product list container
+
+
+  const scrollLeft = () => {
+    if (productListRef.current) {
+      productListRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = () => {
+    if (productListRef.current) {
+      productListRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+    }
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
-      setIsLoading(true); // Start loading before fetching
+      setIsLoading(true);
       try {
         const response = await fetch('https://server-lime-omega.vercel.app/api/products');
         if (!response.ok) {
@@ -18,28 +33,28 @@ const ProductList = () => {
       } catch (error) {
         console.error("Error fetching products:", error);
       }
-      setIsLoading(false); // Stop loading after fetching
+      setIsLoading(false);
     };
 
     fetchProducts();
   }, []);
-
-  const productListStyle = {
-    display: 'grid',
-    padding: '0 var(--padding-lg)',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(280px, auto))',
-    gap: '1rem',
-  };
+  
 
   return (
-    <div className='productList' style={productListStyle}>
-      {isLoading
-        ? Array.from({ length: 8 }).map((_, index) => (
-            <ProductCard key={index} product={null} />
-          ))
-        : products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
+    <div>
+      <div className='productList' ref={productListRef}>
+        {isLoading
+          ? Array.from({ length: 8 }).map((_, index) => (
+              <ProductCard key={index} product={null} />
+            ))
+          : products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+      </div>
+      <div className="indicator">
+        <img id='left' onClick={scrollLeft} src="left-arrow.svg" alt="left-arrow" />
+        <img id='right' onClick={scrollRight} src="right-arrow.svg" alt="right-arrow" />
+      </div>
     </div>
   );
 };
